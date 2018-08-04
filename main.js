91e26c05b8363554fc78438f6b313b4e74f3e2e3
Vlad Haidei <vlad.gaidei@i.ua>
2018-08-04 16:04:46 +0300

    Implemented run of app in tray

--- main.js
+++ main.js
@@ -1,32 +1,95 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu, Tray } = require("electron");
 const path = require("path");
 const url = require("url");
 
+let tray = null;
 let win;
+let isForceClose = false;
 
-function createWindow() {
-    win = new BrowserWindow({ width: 800, height: 600 });
+function appStart() {
+    createWindow();
+    createTray();
+}
 
-    // load the dist folder from Angular
-    win.loadURL(
-        url.format({
-            pathname: path.join(__dirname, `/dist/index.html`),
-            protocol: "file:",
-            slashes: true
-        })
-    );
+function createTray(){
+    // tray
+    tray = new Tray(path.join(__dirname, `/dist/favicon.ico`));
+    tray.setToolTip('Report Organizer');
+    updateTrayMenu();
 
-    win.webContents.openDevTools();
-
-    // The following is optional and will open the DevTools:
-    // win.webContents.openDevTools()
-
-    win.on("closed", () => {
-        win = null;
+    tray.on('click', () => {
+        win.show();
     });
 }
 
-app.on("ready", createWindow);
+function updateTrayMenu() {
+
+    if(tray){
+        const contextMenu = Menu.buildFromTemplate([
+            {
+                label: 'Devtools',
+                type:'checkbox',
+                enabled: !!win,
+                click: toggleDevTools
+            },
+            {
+                label: 'Close',
+                click: () => {
+                    isForceClose = true;
+                    win.close();
+                }
+            }
+        ]);
+        tray.setContextMenu(contextMenu)
+    }
+}
+
+function createWindow() {
+
+    if(!win){
+
+        // set show - true if you need to open window after app start.
+        win = new BrowserWindow({ width: 800, height: 600, show:false });
+
+        // load the dist folder from Angular
+        win.loadURL(
+            url.format({
+                pathname: path.join(__dirname, `/dist/index.html`),
+                protocol: "file:",
+                slashes: true
+            })
+        );
+
+        // The following is optional and will open the DevTools:
+        // win.webContents.openDevTools()
+
+        // hide window instead of close
+        win.on('close', (e) => {
+
+            if(!isForceClose){
+                e.preventDefault();
+                win.hide();
+            }
+        });
+
+        updateTrayMenu();
+    }
+}
+
+function toggleDevTools(){
+    win && win.webContents && win.webContents.toggleDevTools();
+}
+
+function showWindow(){
+
+    if(!win.isVisible()){
+        win && win.show();
+    } else {
+        win && win.hide();
+    }
+}
+
+app.on("ready", appStart);
 
 // on macOS, closing the window doesn't quit the app
 app.on("window-all-closed", () => {
