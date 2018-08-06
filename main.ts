c2608fcddc45dd05356302c4dd54bff2620f29b1
Vlad Haidei <vlad.gaidei@i.ua>
2018-08-06 22:57:22 +0300

    Add system tray functionality

--- main.ts
+++ main.ts
@@ -1,22 +1,22 @@
-import { app, BrowserWindow, Menu, Tray } from 'electron';
+import {app, BrowserWindow, Menu, MenuItem, Tray} from 'electron';
 import * as path from 'path';
 import * as url from 'url';
 
-let tray:Tray = null;
-let win:BrowserWindow = null;
+let tray: Tray = null;
+let win: BrowserWindow = null;
 
 // detect serve mode
 const args = process.argv.slice(1);
-let serve:boolean = args.some(val => val === '--serve');
+const serve: boolean = args.some(val => val === '--serve');
 
-function appStart():void {
+function appStart(): void {
     createWindow();
     createTray();
 }
 
-function createTray(){
+function createTray() {
     // tray
-    if(serve){
+    if (serve) {
         tray = new Tray(path.join(__dirname, `/src/favicon.ico`));
     } else {
         tray = new Tray(path.join(__dirname, `/dist/favicon.ico`));
@@ -25,54 +25,81 @@ function createTray(){
     tray.setToolTip('Report Organizer');
     updateTrayMenu();
 
-    tray.on('click', showWindow);
+    tray.on('double-click', showWindow);
 }
 
 function updateTrayMenu() {
 
-    if(tray){
+    if (tray) {
+
         const contextMenu = Menu.buildFromTemplate([
             {
-                label: 'Devtools',
-                type:'checkbox',
-                enabled: !!win,
-                click: toggleDevTools
+                label: 'Open',
+                click: showWindow
             },
             {
-                label: 'Close',
+                type: 'separator'
+            },
+            {
+                label: 'Add report',
+                click: () => { }
+            },
+            {
+                label: 'Add note',
+                click: () => { }
+            },
+            {
+                label: 'Pause notification',
+                click: () => { }
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Exit',
                 click: () => {
                     app.quit();
                 }
             }
         ]);
-        tray.setContextMenu(contextMenu)
+
+        if (serve) {
+            contextMenu.insert(0, new MenuItem({
+                label: 'DevTools (for dev-build only)',
+                type: 'checkbox',
+                enabled: !!win,
+                click: toggleDevTools
+            }));
+        }
+
+        tray.setContextMenu(contextMenu);
     }
 }
 
 function createWindow() {
-        if(!win){
+
+    if (!win) {
 
         // set show - true if you need to open window after app start.
-        win = new BrowserWindow({ width: 800, height: 600, show:false });
-
+        win = new BrowserWindow({width: 800, height: 600, show: false});
 
         if (serve) {
             // get dynamic version from localhost:4200
             require('electron-reload')(__dirname, {
-                electron: require(`${__dirname}/node_modules/electron`)});
+                electron: require(`${__dirname}/node_modules/electron`)
+            });
             win.loadURL('http://localhost:4200');
         } else {
             // load the dist folder from Angular
             win.loadURL(
                 url.format({
                     pathname: path.join(__dirname, `/dist/index.html`),
-                    protocol: "file:",
+                    protocol: 'file:',
                     slashes: true,
-                    //icon: path.join(__dirname, 'assets/icons/favicon.png')
+                    // icon: path.join(__dirname, 'assets/icons/favicon.png')
                 })
             );
         }
-
 
         // The following is optional and will open the DevTools:
         // win.webContents.openDevTools()
@@ -86,31 +113,31 @@ function createWindow() {
     }
 }
 
-function toggleDevTools(){
-    win && win.webContents && win.webContents.toggleDevTools();
+function toggleDevTools() {
+
+    if (win) {
+        win.webContents.toggleDevTools();
+    }
 }
 
-function showWindow(){
+function showWindow() {
 
-    if(!win){
+    if (!win) {
         createWindow();
     }
 
-    if(!win.isVisible()){
-        win && win.show();
-    } else {
-        win && win.hide();
-    }
-
+    win.show();
 }
 
-app.on("ready", appStart);
+app.on('ready', appStart);
 
 // callback should be empty if we need to leave main process alive after all window close
-app.on("window-all-closed", () => {});
+app.on('window-all-closed', () => {
+
+});
 
 // initialize the app's main window
-app.on("activate", () => {
+app.on('activate', () => {
     if (win === null) {
         createWindow();
     }
