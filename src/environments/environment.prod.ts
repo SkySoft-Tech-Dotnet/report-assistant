44033588850cc373b9d1c4b7f09eb656899b3969
Vlad Haidei <vlad.gaidei@i.ua>
2018-08-09 00:32:01 +0300

    update env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import * as path from 'path';
 
 export const environment = {
     production: true,
-    trayIconPath: path.join(__dirname, '/dist/favicon.ico'),
+    trayIconPath: path.join(__dirname, '../favicon.ico'),
 };
